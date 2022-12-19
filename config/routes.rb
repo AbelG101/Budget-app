0d5aed696caa11902c5167927a7a08bab9f573f2
AbelG101 <abelgebeyehu101@gmail.com>
2022-12-19 17:11:38 +0300

    Add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :purchases
-  resources :categories
-  resources :users
+  resources :users do
+    resources :categories do
+      resources :purchases
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
